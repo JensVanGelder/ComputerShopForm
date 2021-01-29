153fd2c4ec241444684957a2327e53d2eb66621e
JensVanGelder <jens.v.gelder@gmail.com>
2021-01-29 17:12:47 +0100

    added unique Id generation (?)

--- ComputerShopForm/IProduct.cs
+++ ComputerShopForm/IProduct.cs
@@ -2,7 +2,7 @@
 {
     public interface IProduct
     {
-        int Id { get; set; }
+        int Id { get; }
         string Name { get; set; }
         double Price { get; set; }
         string ProductSummary { get; set; }
