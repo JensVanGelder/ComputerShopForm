9da88776c537971bd35066b45d24e43807b78b2a
Simon Van Tittelboom <simon.van.tittelboom@gmail.com>
2021-01-31 14:21:15 +0100

    AddComputerInfo()  tweaks

--- ComputerShopForm/Forms/FormProductInfo.cs
+++ ComputerShopForm/Forms/FormProductInfo.cs
@@ -25,9 +25,9 @@ namespace ComputerShopForm
             txtBoxName.Text = computer.Name;
             txtBoxRam.Text = Convert.ToString(computer.Ram);
             txtBoxMoBo.Text = computer.MoBo;
-            txtBoxPsu.Text = computer.PSU;
-            txtBoxCpu.Text = computer.CPU;
-            txtBoxHdd.Text = computer.HDD;
+            txtBoxPsu.Text = computer.Psu;
+            txtBoxCpu.Text = computer.Cpu;
+            txtBoxHdd.Text = computer.Hdd;
             txtBoxStock.Text = Convert.ToString(computer.Stock);
             txtBoxPrice.Text = Convert.ToString(product.Price);
             txtBoxDescription.Text = product.ProductSummary;
@@ -36,19 +36,17 @@ namespace ComputerShopForm
 
         public void AddTypeInfo(IProduct product)
         {
-            switch (product)
+            if (product is GamingPc)
             {
-                var temp = product as GamingPc;
+                //add GamingPC specifics
             }
             else if (product is Workstation)
             {
-                var temp = product as Workstation;
             }
             else if (product is Laptop)
             {
-                var temp = product as Laptop;
-                txtBoxScreenType.Text = temp.ScreenType;
-                txtBoxWeight.Text = $"{Convert.ToString(temp.WeightInGrams)} grams";
+                //txtBoxScreenType.Text = product.ScreenType;
+                //txtBoxWeight.Text = $"{Convert.ToString(product.WeightInGrams)} grams";
             }
         }
     }
